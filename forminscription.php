6c6126ca4c37d7c12b05f65b0f170b0b59687c62
Aoriah <ifrozenltl@hotmail.com>
2021-12-03 12:21:30 -1100

    haha

--- forminscription.php
+++ forminscription.php
@@ -2,20 +2,28 @@
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 require 'function.php';
+require 'header1.php';
 ?>
 
-<form name="forminscription" method="post" action="inscription.php">
-    <div>             
-        <label for="pseudo">Pseudo:</label>
-        <input value="" id="pseudo" class="mt-2 mb-2" type="text" name="pseudo">
+    <div class="row pt-5">
+        <div class="col-4"><img width="100%"src="https://media3.giphy.com/media/naiatn5LxTOsU/200w.webp?cid=ecf05e47fuwg759pzzqhlp8d83q9v2qjodbawbp4f8bqztkq&rid=200w.webp&ct=g"></div>
+        <div class="col-4 text-center forminscription py-lg-5 py-0">
+            <form name="forminscription" method="post" action="inscription.php">
+                <div class="pt-lg-5">             
+                    <label for="pseudo">Pseudo:</label>
+                    <input value="" style="width:100%;" id="pseudo" class="my-0 my-lg-3" type="text" name="pseudo">
+                </div>
+                <div>             
+                    <label for="email">Email</label>
+                    <input value="" style="width:100%;" id="email" class="my-0 my-lg-3" type="text" name="email">
+                </div>
+                <div>             
+                    <label for="password">Mot de passe:</label>
+                    <input value="" style="width:100%;" id="password" class="my-0 my-lg-3" type="password" name="password">
+                </div>
+                <input id="submit" style="width:100%;" class="mt-4 mb-2" type="submit" value="inscription">
+            </form>
+        </div>
+        <div class="col-4"><img width="100%"src="https://media3.giphy.com/media/naiatn5LxTOsU/200w.webp?cid=ecf05e47fuwg759pzzqhlp8d83q9v2qjodbawbp4f8bqztkq&rid=200w.webp&ct=g"></div>
     </div>
-    <div>             
-        <label for="email">Email</label>
-        <input value="" id="email" class="mt-2 mb-2" type="text" name="email">
-    </div>
-    <div>             
-        <label for="password">Mot de passe:</label>
-        <input value="" id="password" class="mt-2 mb-2" type="password" name="password">
-    </div>
-    <input id="submit" class="mt-2 mb-5 offset-5" type="submit" value="inscription">
-</form>
+
